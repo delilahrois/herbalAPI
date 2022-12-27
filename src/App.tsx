0b2792fbbee1196eb4b4e0ba7baebd020463d193
Delilah Necrason <delilahnecrason@gmail.com>
2022-12-27 11:32:33 -0500

    prettier/ update to es6 import exports and update message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [msg, setMsg] = useState("");
+
+  const getHerbs = () => {
+    fetch("http://localhost:4000/herbs/")
+      .then((res) => res.text())
+      .then((data) => setMsg(data))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => getHerbs()}>welcome!</button>
+      <p>{msg}</p>
     </div>
   );
-}
-
-export default App;
+};
